d592e9d0a0ae23d64d9a089be69f7771402d6407
azhaizhengzhuan <1270780141@qq.com>
2021-04-24 13:04:06 +0800

    练习

---
+++ 2021-3-17/2021-3-17/test.c
@@ -0,0 +1,82 @@
+
+
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("SB\n");
+//	return 0;
+//}
+//int main()
+//{
+//	char ch = 'A';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int age = 20;
+//		printf("%d\n", age);
+//	return 0;
+//}
+//int main()
+//{
+//	long num = 100;
+//	printf("%d\n", num);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+//int main()
+//{
+//	{
+//		int a = 10; 
+//	}
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum=%d\n",sum)
+//	return 0;
+//}
+//int globe = 10;
+//
+//void test()
+//{
+//	printf("test()--globe=%d\n", globe);
+//}
+//int main()
+//{
+//	test();
+//	printf("globe=%d\n", globe);
+//	return 0;
+//}
+//int main()
+//{
+//	extern int g;
+//	printf("%d\n", g);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 10;
+//	scanf("$d", a);
+//	return 0;
+//}
