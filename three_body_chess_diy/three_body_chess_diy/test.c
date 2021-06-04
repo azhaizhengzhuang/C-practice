ca4016989548055464b24d11652810b172082a89
azhaizhengzhuan <1270780141@qq.com>
2021-06-04 14:26:42 +0800

    practice

--- three_body_chess_diy/three_body_chess_diy/test.c
+++ three_body_chess_diy/three_body_chess_diy/test.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include "game.h"
 
-void three_body_chess()
+void three_body_chess(int d)
 {
 	char board[col][row] = { 0 };
 	Init_board(board);//初始化棋盘
@@ -12,51 +12,148 @@ void three_body_chess()
 	{
 		playermove(board);//玩家走
 		print_board(board);//打印棋盘
-	    ret=iswin(board);//判断结果
-		if (ret!='0')
+		ret = iswin(board);//判断结果
+		if (ret != '0')
 			break;
-		computermove(board);//电脑走
+		if (d == 1)
+			computer1move(board);//傻瓜电脑走
+		else if (d == 2)
+			computer2move(board);//正常电脑走
+		else if (d == 3) 
+			computer3move(board);//专家电脑
+		else if (d == 4)//
+			computer4move(board);//冠军电脑
+		else
+			Jamesmove(board);//姆斯电脑
 		print_board(board);//打印棋盘
-		ret=iswin(board);//判断结果
-		if (ret!='0')
+		ret = iswin(board);//判断结果
+		if (ret != '0')
 			break;
 	}
-	if (ret == '*')
+	if (d == 1)
 	{
-		printf("打赢一个傻子也没什么好骄傲的\n\n");
+		if (ret == '*')
+		{
+			printf("猩猩哥：“我输给你是要引起你的注意力，其实我喜欢你很久了”\n\n");
+		}
+		else if (ret == '@')
+		{
+			printf("猩猩哥：“来做我的压寨夫人吧。”\n\n");
+		}
+		else
+		{
+			printf("猩猩哥：“简简单单。”\n\n");
+		}
 	}
-	else if (ret == '@')
+	else if (d == 2)
 	{
-		printf("憨批 连傻子都打不赢\n\n");
+		if (ret == '*')
+		{
+			printf("枫：“什么高玩。”\n\n");
+		}
+		else if (ret == '@')
+		{
+			printf("枫：“再接再厉。”\n\n");
+		}
+		else
+		{
+			printf("枫：“哈哈哈 平局了。”\n\n");
+		}
+	}
+	else if (d == 3)
+	{
+		if (ret == '*')
+		{
+			printf("锅锅：“你的妈妈的，再来一局。”\n\n");
+		}
+		else if (ret == '@')
+		{
+			printf("锅锅：“输给我很正常。”\n\n");
+		}
+		else
+		{
+			printf("锅锅：“也就那样，三子棋谁来都平局。”\n\n");
+		}
+	}
+	else if (d == 4)
+	{
+		if (ret == '*')
+		{
+			printf("兰特：“你是不是作弊了？”\n\n");
+		}
+		else if (ret == '@')
+		{
+			printf("兰特：“哈哈，sb。”\n\n");
+		}
+		else
+		{
+			printf("兰特：“平局很正常的。”\n\n");
+		}
 	}
 	else
 	{
-		printf("跟傻子电脑都能打平 真有你的\n\n");
+		if (ret == '*')
+		{
+			printf("姆斯：“你的妈妈的，再来一局。”\n\n");
+		}
+		else if (ret == '@')
+		{
+			printf("姆斯：“虽然你输了，但是我赢了你这么多子，晚上请你吃烤鱼吧。”\n\n");
+		}
+		else
+		{
+			printf("姆斯：“谁都赢不了的。”\n\n");
+		}
 	}
 }
 
 void game()
 {
 	int input = 0;
-	do
+	while (1)
 	{
-		menu();
-		printf("请选择：>");
-		scanf("%d", &input);
-		switch (input)
+		do
 		{
-		case 0:
-			printf("退出游戏\n");
-			break;
-		case 1:
-			three_body_chess();
-			break;
-		default:
-			printf("输入错误，请重新输入\n");
-			break;
-		}
+			menu();
+			printf("请选择：>");
+			scanf("%d", &input);
+			switch (input)
+			{
+			case 1:
+				break;
+			default:
+				printf("旁白：“你别跟我扯那些有的没的。”\n");
+				break;
+			}
+		} while (input != 1);
+		do
+		{
 
-	} while (input);
+			difficulty();
+			printf("请输入对应的数字：>");
+			scanf("%d", &input);
+			switch (input)
+			{
+			case 1:
+				three_body_chess(input);
+				break;
+			case 2:
+				three_body_chess(input);
+				break;
+			case 3:
+				three_body_chess(input);
+				break;
+			case 4:
+				three_body_chess(input);
+				break;
+			case 5:
+				three_body_chess(input);
+			default:
+				printf("\n旁白：“反正这件事你安排就对了。”\n");
+				break;
+			}
+		} while ((input != 1) && (input != 2) && (input != 3));
+	}
 }
 
 int main()
